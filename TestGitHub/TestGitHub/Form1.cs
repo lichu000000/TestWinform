aa78b0e23f7c46dd1ca5ab96be39e9f28599c02b
dell <dell@DESKTOP-OFJD18E>
2019-11-07 16:30:34 +0800

    测试克隆 修改

--- TestGitHub/TestGitHub/Form1.cs
+++ TestGitHub/TestGitHub/Form1.cs
@@ -15,5 +15,10 @@ namespace TestGitHub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("创建成功");
+        }
     }
 }
